a54e036cbe9e32a02a5a3c83f3a24dccb223d6ab
Julian Garcia <julian.garcia@pobox.com>
2012-12-12 17:07:45 +0100

    Remove agent-based modules, which will go to another project.

--- jevodyn/src/test/java/com/evolutionandgames/jevodyn/AllTests.java
+++ jevodyn/src/test/java/com/evolutionandgames/jevodyn/AllTests.java
@@ -4,15 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.evolutionandgames.jevodyn.agentbased.AgentBasedSimulationDistributionTest;
-import com.evolutionandgames.jevodyn.agentbased.AgentBasedSimulationFixationTest;
-import com.evolutionandgames.jevodyn.agentbased.AgentBasedSimulationTimeSeriesTest;
-import com.evolutionandgames.jevodyn.agentbased.AgentBasedSimulationTotalPayoffTest;
-import com.evolutionandgames.jevodyn.agentbased.impl.AgentBasedPopulationImplTest;
-import com.evolutionandgames.jevodyn.agentbased.impl.AgentBasedWrightFisherProcessWithAssortmentTest;
-import com.evolutionandgames.jevodyn.agentbased.impl.AgentMutatorSimpleKernelTest;
-import com.evolutionandgames.jevodyn.agentbased.simple.AgentBasedSimplePopulationFactoryTest;
-import com.evolutionandgames.jevodyn.agentbased.simple.AgentBasedSimpleRandomPopulationFactoryTest;
 import com.evolutionandgames.jevodyn.stochastic.SimulationFixationProbabilityTest;
 import com.evolutionandgames.jevodyn.stochastic.SimulationStationaryDistributionTest;
 import com.evolutionandgames.jevodyn.stochastic.SimulationTimeSeriesTest;
@@ -31,16 +22,7 @@ import com.evolutionandgames.jevodyn.utils.RandomTest;
 			   RandomTest.class, 
 			   SimulationStationaryDistributionTest.class,
 			   SimulationTimeSeriesTest.class,
-			   SimulationTotalPayoffTest.class,
-			   AgentBasedPopulationImplTest.class,
-			   AgentBasedWrightFisherProcessWithAssortmentTest.class,
-			   AgentBasedSimulationTimeSeriesTest.class,
-			   AgentBasedSimulationFixationTest.class,
-			   AgentBasedSimulationDistributionTest.class,
-			   AgentBasedSimulationTotalPayoffTest.class,
-			   AgentBasedSimplePopulationFactoryTest.class,
-			   AgentBasedSimpleRandomPopulationFactoryTest.class,
-			   AgentMutatorSimpleKernelTest.class})
+			   SimulationTotalPayoffTest.class})
 
 public class AllTests {
 
