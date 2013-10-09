56a98df70b42d1fbaea8c717cf7c32b530c42445
Julian Garcia <julian.garcia@pobox.com>
2013-10-09 22:29:55 +0200

    added fast forward methods for estimation and step with geometric distribution sampling to speed up computation

--- jevodyn/src/main/java/com/evolutionandgames/jevodyn/dimorphic/DimorphicMoranProcess.java
+++ jevodyn/src/main/java/com/evolutionandgames/jevodyn/dimorphic/DimorphicMoranProcess.java
@@ -170,5 +170,114 @@ public class DimorphicMoranProcess {
 		}
 		return ans;
 	}
+	
+	
+	public int stepFastForward() {
+        double[] payoffs = this.payoffCalculator.payoff(this.population);
+        double fitnessResident = this.population.getNumberOfResidents()
+                * mapFitness(payoffs[0], this.mapping);
+        double fitnessMutant = this.population.getNumberOfMutants()
+                * mapFitness(payoffs[1], this.mapping);
+        double p = fitnessMutant / (fitnessResident + fitnessMutant);// mutantSelectedProbability
+        double q = this.population.getMutantFrequency();//mutantForDeathProbability
+        
+        double probabilityMutantIncrement = p*(1.0-q);
+        double probabilityMutantDecrement = q*(1.0-p);
+        double probabilitySomethingHappehs = probabilityMutantDecrement + probabilityMutantIncrement;
+        
+        int timeStepsUntilSomethingHappens = Random.simulateGeometricDistribution(probabilitySomethingHappehs);
+        
+        if (Random.bernoulliTrial(probabilityMutantIncrement/probabilitySomethingHappehs)) {
+            this.population.incrementNumberOfMutants();
+        }else{
+            this.population.decrementNumberOfMutants();
+        }
+        this.timeStep= timeStep + timeStepsUntilSomethingHappens +1;
+        return timeStepsUntilSomethingHappens +1;
+}
+
+
+private void burnFastForward(int burningTimePerEstimate) {
+        int burningStep = 0;
+        if (!this.population.isFixated())
+            throw new IllegalStateException(
+                    "This method assumes a fixated population");
+        while (burningStep < burningTimePerEstimate) {
+            // compute and add time to escape
+            int residentType = this.population.getTypeOfResident();
+            double escapeProbability = 1.0 - this.mutationKernel.getEntry(
+                    residentType, residentType);
+            int escapeTime = Random
+                    .simulateGeometricDistribution(escapeProbability);
+            burningStep = burningStep + escapeTime;
+            // add the mutant
+            double[] distibutionGivenThatIJumpedOut = transformToConditional(
+                    mutationKernel.getRow(residentType), residentType);
+            int mutantType = Random
+                    .simulateDiscreteDistribution(distibutionGivenThatIJumpedOut);
+            this.population.introduceNewMutant(mutantType);
+            // if burning time is not over, run to fixation or until burning
+            // time is over
+            while (!this.population.isFixated()
+                    && burningStep < burningTimePerEstimate) {
+                int steps = this.stepFastForward();
+                burningStep+=steps;
+            }
+        }
+    }
+
+
+public double[] estimateStationaryDistributionSmallMutationFastForward(
+            int burningTimePerEstimate, int samplesPerEstimate,
+            int numberOfEstimates, Long seed) {
+        Random.seed(seed);
+        int numberOfTypes = this.population.getNumberOfTypes();
+        int populationSize = this.population.getSize();
+        long[] countPerStrategy = new long[numberOfTypes];
+        for (int estimate = 0; estimate < numberOfEstimates; estimate++) {
+            // for every new esimate start at a random type
+            int typeOfResident = Random.nextInt(numberOfTypes);
+            this.population = new DimorphicPopulation(populationSize,
+                    typeOfResident, numberOfTypes);
+            this.timeStep = 0;
+            // burning time
+            this.burnFastForward(burningTimePerEstimate);
+            int samples = 0;
+            int timeSpent = 1;
+            // run to fixation and count
+            while (samples < samplesPerEstimate) {
+                while (!this.population.isFixated()) {
+                    countPerStrategy[this.population.getTypeOfResident()] = countPerStrategy[this.population
+                            .getTypeOfResident()]
+                            + timeSpent*this.population.getNumberOfResidents();
+                    countPerStrategy[this.population.getTypeOfMutant()] = countPerStrategy[this.population
+                            .getTypeOfMutant()]
+                            + timeSpent*this.population.getNumberOfMutants();
+                    timeSpent = this.stepFastForward();
+                    samples+=timeSpent;
+                }
+                if (samples > samplesPerEstimate)
+                    break;
+                // here we are fixated
+                int residentType = this.population.getTypeOfResident();
+                double escapeProbability = 1.0 - this.mutationKernel.getEntry(
+                        residentType, residentType);
+                int escapeTime = Random
+                        .simulateGeometricDistribution(escapeProbability);
+                samples = samples + escapeTime;
+                countPerStrategy[this.population.getTypeOfResident()] = countPerStrategy[this.population
+                        .getTypeOfResident()]
+                        + escapeTime
+                        * this.population.getSize();
+                // add the mutant
+                double[] distibutionGivenThatIJumpedOut = transformToConditional(
+                        mutationKernel.getRow(residentType), residentType);
+                int mutantType = Random
+                        .simulateDiscreteDistribution(distibutionGivenThatIJumpedOut);
+                this.population.introduceNewMutant(mutantType);
+            }
+        }
+        return ArrayUtils.normalize(countPerStrategy);
+    }
 
 }
--- jevodyn/src/test/java/com/evolutionandgames/jevodyn/StationaryDistributionMoranProcessSmallMutationTest.java
+++ jevodyn/src/test/java/com/evolutionandgames/jevodyn/StationaryDistributionMoranProcessSmallMutationTest.java
@@ -44,5 +44,35 @@ public class StationaryDistributionMoranProcessSmallMutationTest {
 		assertEquals(0.25169438, ans[2], DELTA);
 	}
 	
+	
+	@Test
+	public void testSimulateStationaryDistributionNeutralFastForward() {
+		Long seed = System.currentTimeMillis();
+		DimorphicPopulation population = new DimorphicPopulation(10,0,3);
+		DimorphicMoranProcess mp  = new DimorphicMoranProcess(population, PayoffToFitnessMapping.EXPONENTIAL, 0.0, 0.01, new GamePayoffCalculator(Games.allcTftAlld()));
+		int burningTimePerEstimate = 100;
+		int samplesPerEstimate = 5000000;
+		int numberOfEstimates = 5;
+		double[] ans = mp.estimateStationaryDistributionSmallMutationFastForward(burningTimePerEstimate, samplesPerEstimate, numberOfEstimates, seed);
+		for (int i = 0; i < ans.length; i++) {
+			assertEquals(1.0 / population.getNumberOfTypes(), ans[i], DELTA);
+		}
+	}
+	
+	
+	@Test
+	public void testSimulateStationaryDistributionFastForward() {
+		Long seed = System.currentTimeMillis();
+		DimorphicPopulation population = new DimorphicPopulation(10,0,3);
+		DimorphicMoranProcess mp  = new DimorphicMoranProcess(population, PayoffToFitnessMapping.EXPONENTIAL, 1.0, 0.001, new GamePayoffCalculator(Games.allcTftAlld()));
+		int burningTimePerEstimate = 1000;
+		int samplesPerEstimate = 5000000;
+		int numberOfEstimates = 100;
+		double[] ans = mp.estimateStationaryDistributionSmallMutationFastForward(burningTimePerEstimate, samplesPerEstimate, numberOfEstimates, seed);
+		assertEquals(0.0799139, ans[0], DELTA);
+		assertEquals(0.66839172, ans[1], DELTA);
+		assertEquals(0.25169438, ans[2], DELTA);
+	}
+	
 
 }
