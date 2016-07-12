bf10a2f627804896e0d2f8149c67ed49c7581545
Julian Garcia <julian.garcia@monash.edu>
2016-07-12 14:49:20 +0200

    Fast version of Moran process ignores Monomorphous populations

--- jevodyn/src/test/java/com/evolutionandgames/jevodyn/SimulationFixationProbabilityTest.java
+++ jevodyn/src/test/java/com/evolutionandgames/jevodyn/SimulationFixationProbabilityTest.java
@@ -4,9 +4,9 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import com.evolutionandgames.jevodyn.Simulation;
 import com.evolutionandgames.jevodyn.impl.GamePayoffCalculator;
 import com.evolutionandgames.jevodyn.impl.MoranProcess;
+import com.evolutionandgames.jevodyn.impl.MoranProcessFast;
 import com.evolutionandgames.jevodyn.impl.SimplePopulationImpl;
 import com.evolutionandgames.jevodyn.utils.Games;
 
@@ -36,5 +36,28 @@ public class SimulationFixationProbabilityTest extends TestCase {
 		double estimate = simulationFixationProbability.estimateFixationProbability(1, 0, 1000000, seed);
 		assertEquals(0.7364040545619178, estimate, DELTA);	
 	}
+	
+	@Test
+	public void testEstimateNeutralMutantFast() {
+		int[] array = {5,5};
+		int popSize =10;
+		Long seed = System.currentTimeMillis();
+		//System.out.println(seed);
+		MoranProcess mp = new MoranProcessFast(new SimplePopulationImpl(array), new GamePayoffCalculator(Games.hawkDoveGame()), 0.1, 0.0);
+		Simulation simulationFixationProbability = new Simulation(mp);
+		double estimate = simulationFixationProbability.estimateFixationProbability(0, 1, 1000000, seed);
+		assertEquals(1.0/popSize, estimate, DELTA);	
+	}
+	
+	@Test
+	public void testNonTrivialFixationFast() {
+		int[] array = {5,5};
+		Long seed = System.currentTimeMillis();
+		MoranProcess mp = new MoranProcessFast(new SimplePopulationImpl(array), new GamePayoffCalculator(Games.prionersDilemma()), 0.1, 1.0);
+		Simulation simulationFixationProbability = new Simulation(mp);
+		double estimate = simulationFixationProbability.estimateFixationProbability(1, 0, 1000000, seed);
+		assertEquals(0.7364040545619178, estimate, DELTA);	
+	}
+
 
 }
